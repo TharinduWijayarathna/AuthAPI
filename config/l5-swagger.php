224995efccbd229566df05269cf50203fa52d75e
TharinduWijayarathna <wikum.dev@gmail.com>
2024-11-28 12:28:58 +0530

    Fix:Swagger doc page title updated

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,7 +5,7 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'L5 Swagger UI',
+                'title' => 'PatPat API Documentation',
             ],
 
             'routes' => [
